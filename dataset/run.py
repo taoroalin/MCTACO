fa14a8b4d4a03cb4a13bec2e8d60a319955fd354
Tao Lin <taoroalin@gmail.com>
2023-03-25 12:43:25 -0700

    more wrong examples

--- dataset/run.py
+++ dataset/run.py
@@ -25,7 +25,14 @@ def question_to_pair(question):
     options ="\n".join([f"{i} {x[0]}" for i,x in enumerate(question[1])])
     answers = " ".join([str(i)+" "+x[1] for i,x in enumerate(question[1])])
     return [{"role":"user","content":f"{question[0][0]}\n{question[0][1]}\n{options}"},{"role":"assistant","content":answers}]
-sys_prompt = {"role": "system", "content": "Your job is to win the MCTACO Your job is to classify which answers to natural language questions make sense. You will see some background context, a question, and 1 to 15 possible answers. You have to classify which ones make sense as answers to the question. Write each number followed by the classification 'yes' or 'no' seperated by spaces with no discussion or explanation. When there are multiple phrasings of the same answer, for instance 'yes, it was' and 'yes', or '24 hours' and '1 day', you must answer them all consistently. Sometimes there are no plausible answers, in which case you say no to all of them."}
+sys_prompt = {"role": "system", "content": """Your job is to classify which answers to natural language would be labelled as correct by most clould workers (eg MTurk workers). You will see some background context, a question, and 1 to 15 possible answers. 
+
+Some advice based on your past mistakes:
+
+* When there are multiple phrasings of the same answer, for instance 'yes, it was' and 'yes', or '24 hours' and '1 day', you must answer them all consistently
+ 
+* If there are no good answers, just say no to all of them.
+"""}
 def generate_openai_chat(shot_questions, question):
     response = openai.ChatCompletion.create(
             model="gpt-4",# "gpt-3.5-turbo"
@@ -67,7 +74,7 @@ def evaluate():
     num_right=0
     false_n = 0
     false_p = 0
-    pbar = tqdm(enumerate(data_dev[k:]))
+    pbar = tqdm(list(enumerate(data_dev[k:])))
     for i,question in pbar:
         openai_response = generate_middleman(few_shots,question)
         print(openai_response)
@@ -84,10 +91,10 @@ def evaluate():
         if len(wrong_examples)%10==0 and len(wrong_examples)!=0:
             open("wrong_examples","w").write(str(wrong_examples))
             
-        if i%50==0 and i!=0:
-            open(outfilename,"w").write("\n".join(responses))
+        # if i%50==0 and i!=0:
+        #     open(outfilename,"w").write("\n".join(responses))
         pbar.set_description(f"{num_right/(i+1)} fp {false_p} fn {false_n}")
-    open(outfilename,"w").write("\n".join(responses))
+    # open(outfilename,"w").write("\n".join(responses))
 def run():
     pbar = enumerate(tqdm(data_test[len(responses):]))
     for i,datum in pbar:
